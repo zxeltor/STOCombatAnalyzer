ce5afd26e910424531953d12af9317d67c53ecd1
Zxeltor <todd.taylor.ee@gmail.com>
2024-06-24 14:25:27 -0400

    Add a helper method to launch the OS default browser with a Url

--- zxeltor.ConfigUtilsHelpers/Helpers/UrlHelper.cs
+++ zxeltor.ConfigUtilsHelpers/Helpers/UrlHelper.cs
@@ -4,6 +4,8 @@
 // This source code is licensed under the Apache-2.0-style license found in the
 // LICENSE file in the root directory of this source tree.
 
+using System.Diagnostics;
+
 namespace zxeltor.ConfigUtilsHelpers.Helpers;
 
 /// <summary>
@@ -16,7 +18,8 @@ public static class UrlHelper
     /// <summary>
     ///     Attempt to combine string segments using forward slashes between each segment to make a valid Url.
     ///     <para>
-    ///         Note: This is a simple string join, using a forward slash between each segment. If trying to construct an absolute Url with http protocol,
+    ///         Note: This is a simple string join, using a forward slash between each segment. If trying to construct an
+    ///         absolute Url with http protocol,
     ///         host, and port (i.e. http://localhost:90), they all need to be in included in the first segment.
     ///     </para>
     /// </summary>
@@ -26,6 +29,15 @@ public static class UrlHelper
     {
         var segmentsResult = string.Join("/", segments.Select(seg => seg.Trim().Trim('/')));
         return segmentsResult;
+    }
+
+    /// <summary>
+    ///     Attempt to launch the OS default browser with the supplied url.
+    /// </summary>
+    /// <param name="url">The URL to navigate too.</param>
+    public static void LaunchUrlInDefaultBrowser(string url)
+    {
+        using var process = Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
     }
 
     #endregion
