2e45b05c70f61a7631ecaf0baa70d6af53bf8fcf
Zxeltor <todd.taylor.ee@gmail.com>
2024-08-29 13:49:14 -0400

    Added new items to the types lib. A new EmptyCollectionException, and LargeObservableCollection.

---
+++ zxeltor.Types.Lib/Collections/LargeObservableCollection.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2024, Todd Taylor (https://github.com/zxeltor)
+// All rights reserved.
+// 
+// This source code is licensed under the Apache-2.0-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace zxeltor.Types.Lib.Collections;
+
+/// <summary>
+///     This class is an implementation of <see cref="ObservableCollection{T}" />.
+///     <para>Note: This class provides an AddRange method to add multiple items at once.</para>
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class LargeObservableCollection<T> : ObservableCollection<T>
+{
+    #region Constructors
+
+    /// <summary>
+    ///     An implementation to initialize the base class constructor.
+    ///     <see cref="ObservableCollection{T}" />
+    /// </summary>
+    public LargeObservableCollection()
+    {
+    }
+
+    /// <summary>
+    ///     An implementation to initialize the base class constructor.
+    ///     <see cref="ObservableCollection{T}" />
+    /// </summary>
+    public LargeObservableCollection(IEnumerable<T> enumerable) : base(enumerable)
+    {
+    }
+
+    /// <summary>
+    ///     An implementation to initialize the base class constructor.
+    ///     <see cref="ObservableCollection{T}()" />
+    /// </summary>
+    public LargeObservableCollection(List<T> list) : base(list)
+    {
+    }
+
+    #endregion
+
+    #region Public Members
+
+    /// <summary>
+    ///     Add a collection of <see cref="IEnumerable{T}" /> to the end of the existing collection
+    ///     <para>
+    ///         Note: The method doesn't trigger the <see cref="INotifyCollectionChanged" /> event until after all items have
+    ///         been
+    ///         added.
+    ///     </para>
+    /// </summary>
+    /// <param name="collection">The collection of items to add.</param>
+    public void AddRange(IEnumerable<T> collection)
+    {
+        foreach (var i in collection) this.Items.Add(i);
+        this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+
+    #endregion
+}
---
+++ zxeltor.Types.Lib/Exceptions/CollectionEmptyException.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2024, Todd Taylor (https://github.com/zxeltor)
+// All rights reserved.
+// 
+// This source code is licensed under the Apache-2.0-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+namespace zxeltor.Types.Lib.Exceptions;
+
+/// <summary>
+///     An exception to throw when you can't handle an empty collection.
+/// </summary>
+public class CollectionEmptyException : Exception
+{
+    #region Constructors
+    /// <inheritdoc />
+    public CollectionEmptyException()
+    {
+    }
+
+    /// <inheritdoc />
+    public CollectionEmptyException(string memberName) : base()
+    {
+        MemberName = memberName;
+    }
+
+    /// <inheritdoc />
+    public CollectionEmptyException(string message, string memberName) : base(message)
+    {
+        this.MemberName = memberName;
+    }
+
+    /// <inheritdoc />
+    public CollectionEmptyException(string message, string memberName, Exception exception) : base(message)
+    {
+        this.MemberName = memberName;
+    }
+
+    /// <inheritdoc />
+    public CollectionEmptyException(string memberName, Exception exception) : base(exception.Message, exception)
+    {
+        this.MemberName = memberName;
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    /// <summary>
+    ///     The member name of the offending collection.
+    /// </summary>
+    public string? MemberName { get; set; }
+
+    #endregion
+}
