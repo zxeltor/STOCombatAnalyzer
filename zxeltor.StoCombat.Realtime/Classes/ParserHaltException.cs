ef26bb204926ece269aebde568d0f1e4cbd6ff8c
Zxeltor <todd.taylor.ee@gmail.com>
2024-09-27 00:57:04 -0400

    Cleanup repo after rework for v2.1.0

--- zxeltor.StoCombat.Realtime/Classes/ParserHaltException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace zxeltor.StoCombat.Realtime.Classes
-{
-    public class ParserHaltException : Exception
-    {
-        public ParserHaltException() : base() { }
-        public ParserHaltException(string message) : base(message) { }
-        public ParserHaltException(string message, Exception e) : base(message, e) { }
-    }
-}
